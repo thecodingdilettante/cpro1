46dd411eefc952340327e2db997c802c88eea3e2
thecodingdilettante <forbesm@usf.edu>
2025-10-14 13:47:00 -0400

    Part 3

--- cpro_1.js
+++ cpro_1.js
@@ -1,14 +1,30 @@
-function changeText() {
-  const CTA = document.getElementById("myCallToAction");
-  CTA.textContent = "You're ready to go!";
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltip = document.createElement("div");
+  tooltip.className = "tooltip";
+  document.body.appendChild(tooltip);
 
-const input = document.querySelector("input");
-const result = document.getElementById("result");
+  document.querySelectorAll("[data-tooltip]").forEach(elem => {
+    elem.addEventListener("mouseover", function() {
+      tooltip.textContent = this.dataset.tooltip;
+      const rect = this.getBoundingClientRect();
+      tooltip.style.left = rect.left + window.scrollX + "px";
+      tooltip.style.top = rect.bottom + window.scrollY + "px";
+      tooltip.style.display = "block";
+    });
 
-input.addEventListener("change", updateValue);
+    elem.addEventListener("mouseout", function() {
+      tooltip.style.display = "none";
+    });
+  });
+});
 
-function updateValue(e) {
-  result.textContent = e.target.value;
-}
+const email = document.getElementById("message");
+
+message.addEventListener("input", (event) => {
+  if (message.validity.tooShort) {
+    message.setCustomValidity("Please write at least 30 characters!");
+  } else {
+    message.setCustomValidity("");
+  }
+});
 
